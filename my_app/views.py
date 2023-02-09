61a9e307b73449e618028ece56902cf3d4f27234
Dmytro Petliovanyi <petliovany@gmail.com>
2023-02-09 13:38:59 +0200

    threads resolve

--- my_app/views.py
+++ my_app/views.py
@@ -1,38 +1,36 @@
 import os
 
-from flask import render_template, request
+from flask import Response, redirect, render_template, request
 from report_of_monaco_racing import get_racer, groper
 
 from my_app import app
-from my_app.functions_view import form_racers, upload_files
+from my_app.functions_view import form_racers
 
 
 @app.route("/")
-def main():
-    return render_template("main.html")
-
-
-@app.route("/report", methods=["POST", "GET"])
+@app.route("/report")
 def report() -> str:
-    upload_files()
-
     return render_template("report.html")
 
 
 @app.route("/report/drivers")
 def drivers() -> str:
-    racers = form_racers()
-    return render_template("drivers.html", racers=racers)
+    args = request.args.to_dict()
+
+    if args.get("order") == "desc":
+        return render_template("drivers.html", racers=form_racers(True))
+
+    if not args or args.get("order") == "asc":
+        return render_template("drivers.html", racers=form_racers())
 
 
-@app.route("/report/drivers_desc")
-def drivers_desc() -> str:
-    racers = form_racers(True)
-    return render_template("drivers.html", racers=racers)
-
-
-@app.route("/handle_data", methods=["POST"])
-def handle_data() -> str:
-    fullname = request.form["fullname"]
-    racer_info = str(get_racer(groper(f"{os.getcwd()}/race_info"), fullname))
+@app.route("/report/drivers/driver_id=<driver_name>")
+def single_driver(driver_name: str) -> str:
+    racer_info = str(get_racer(groper(os.environ["TARGET_DIR"]), driver_name))
     return render_template("driver_info.html", racer_info=racer_info)
+
+
+@app.route("/report/drivers/driver_handle", methods=["POST"])
+def handle_data() -> Response:
+    fullname = request.form["fullname"]
+    return redirect(f"/report/drivers/driver_id={fullname}")
